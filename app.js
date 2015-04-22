d62b34c38ba1862d3922c0058f5634cd252b8c41
johnytiago <joaosao10@gmail.com>
2015-04-22 07:29:56 -0400

    back again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ 
     host: 'localhost', 
-    port: 80 
+    port: 8080
 });
 var path = require('path');
 
@@ -28,3 +28,4 @@ server.route({
 server.start(function(){
 	console.log('Server running at: ', server.info.uri);
 });
+
